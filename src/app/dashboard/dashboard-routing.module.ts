44520a6654243d0fa4c9d528614c3dfe77b571c9
Tal Yo <tal@analink.com>
2022-05-03 08:57:46 -0400

    updated dashboard and parties

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -7,63 +7,79 @@ const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    children: [
+      {
+        path: 'admin',
+        loadChildren: () =>
+          import('./admin/admin.module').then((m) => m.AdminModule),
+      },
+      {
+        path: 'importers',
+        loadChildren: () =>
+          import('./importers/importers.module').then((m) => m.ImportersModule),
+      },
+      {
+        path: 'exporters',
+        loadChildren: () =>
+          import('./exporters/exporters.module').then((m) => m.ExportersModule),
+      },
+      {
+        path: 'forwarders',
+        loadChildren: () =>
+          import('./forwarders/forwarders.module').then(
+            (m) => m.ForwardersModule
+          ),
+      },
+      {
+        path: 'truckers',
+        loadChildren: () =>
+          import('./truckers/truckers.module').then((m) => m.TruckersModule),
+      },
+      {
+        path: 'cfs-terminal-oper',
+        loadChildren: () =>
+          import('./cfs-terminal-oper/cfs-terminal-oper.module').then(
+            (m) => m.CfsTerminalOperModule
+          ),
+      },
+      {
+        path: 'air-carriers',
+        loadChildren: () =>
+          import('./air-carriers/air-carriers.module').then(
+            (m) => m.AirCarriersModule
+          ),
+      },
+      {
+        path: 'sea-carriers',
+        loadChildren: () =>
+          import('./sea-carriers/sea-carriers.module').then(
+            (m) => m.SeaCarriersModule
+          ),
+      },
+      {
+        path: 'service-providers',
+        loadChildren: () =>
+          import('./service-providers/service-providers.module').then(
+            (m) => m.ServiceProvidersModule
+          ),
+      },
+      {
+        path: 'users',
+        loadChildren: () =>
+          import('./users/users.module').then((m) => m.UsersModule),
+      },
+      {
+        path: 'managers',
+        loadChildren: () =>
+          import('./managers/managers.module').then((m) => m.ManagersModule),
+      },
+      {
+        path: 'shippers',
+        loadChildren: () =>
+          import('./shippers/shippers.module').then((m) => m.ShippersModule),
+      },
+    ],
   },
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./admin/admin.module').then((m) => m.AdminModule),
-  },
-  {
-    path: 'importers',
-    loadChildren: () =>
-      import('./importers/importers.module').then((m) => m.ImportersModule),
-  },
-  {
-    path: 'exporters',
-    loadChildren: () =>
-      import('./exporters/exporters.module').then((m) => m.ExportersModule),
-  },
-  {
-    path: 'forwarders',
-    loadChildren: () =>
-      import('./forwarders/forwarders.module').then((m) => m.ForwardersModule),
-  },
-  {
-    path: 'truckers',
-    loadChildren: () =>
-      import('./truckers/truckers.module').then((m) => m.TruckersModule),
-  },
-  {
-    path: 'cfs-terminal-oper',
-    loadChildren: () =>
-      import('./cfs-terminal-oper/cfs-terminal-oper.module').then(
-        (m) => m.CfsTerminalOperModule
-      ),
-  },
-  {
-    path: 'air-carriers',
-    loadChildren: () =>
-      import('./air-carriers/air-carriers.module').then(
-        (m) => m.AirCarriersModule
-      ),
-  },
-  {
-    path: 'sea-carriers',
-    loadChildren: () =>
-      import('./sea-carriers/sea-carriers.module').then(
-        (m) => m.SeaCarriersModule
-      ),
-  },
-  {
-    path: 'service-providers',
-    loadChildren: () =>
-      import('./service-providers/service-providers.module').then(
-        (m) => m.ServiceProvidersModule
-      ),
-  },
-  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
-  { path: 'managers', loadChildren: () => import('./managers/managers.module').then(m => m.ManagersModule) },
-  { path: 'shippers', loadChildren: () => import('./shippers/shippers.module').then(m => m.ShippersModule) },
 ];
 
 @NgModule({
--- src/app/parties/parties-routing.module.ts
+++ src/app/parties/parties-routing.module.ts
@@ -1,11 +1,34 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PartiesComponent } from './parties.component';
 
-const routes: Routes = [{ path: '', component: PartiesComponent }];
+import { DomesticComponent } from './domestic/domestic.component';
+import { ForeignComponent } from './foreign/foreign.component';
+import { NgModule } from '@angular/core';
+import { PartiesComponent } from './parties.component';
+import { ServiceProvidersComponent } from './service-providers/service-providers.component';
+import { TruckersComponent } from './truckers/truckers.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PartiesComponent,
+    children: [
+      { path: 'domestic', component: DomesticComponent },
+      { path: 'foreign', component: ForeignComponent },
+      { path: 'service-providers', component: ServiceProvidersComponent },
+      { path: 'truckers', component: TruckersComponent },
+    ],
+  },
+];
 
 @NgModule({
+  declarations: [
+    PartiesComponent,
+    DomesticComponent,
+    ForeignComponent,
+    TruckersComponent,
+    ServiceProvidersComponent,
+  ],
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PartiesRoutingModule { }
+export class PartiesRoutingModule {}
